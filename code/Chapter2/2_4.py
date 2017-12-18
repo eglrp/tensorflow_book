525ceb0c269f707aee943ae6b92df7ac82a40ab7
panda404 <13342298293@163.com>
2017-12-18 15:36:03 +0800

    201712181535

--- code/Chapter2/2_4.py
+++ code/Chapter2/2_4.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
-X = tf.placeholder(tf.float32, [None, 5])
+x = tf.placeholder(tf.float32, [None, 5])
 ep_ = tf.placeholder(tf.float32, [None, 1])
-theta = tf.Variable(tf.zeros([5, 1]))
-ep = tf,matmul(X,theta)
+theta = tf.Variable(tf.ones([5, 1]))
+ep = tf.matmul(x,theta)
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+print(sess.run(ep,feed_dict={x:[[1,2,3,4,5]]}))
